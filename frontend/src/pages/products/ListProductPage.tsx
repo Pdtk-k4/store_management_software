37211f5e00123ab2f0ac416e270805c8eee5226e
NguyenDuc1003 <ducmvb@gmail.com>
2025-10-02 14:41:44 +0700

    up

--- frontend/src/pages/products/ListProductPage.tsx
+++ frontend/src/pages/products/ListProductPage.tsx
@@ -8,101 +8,104 @@ import ListProduct from "../products/components/ListProduct";
 import ProductTable from "../products/components/ProductTable";
 const ListProductPage: React.FC = () => {
   return (
-    <div className="bg-white space-y-2 p-4 rounded-md">
-      <div>
+    <div className="p-4  bg-gray-100 lg:p-6">
+      <div className="bg-white space-y-2  p-4 rounded-md">
         <div>
-          <p className="text-left m-px mt-2">
-            <h3 className="text-left m-px">Danh sách sản phẩm</h3>
-            Quản lý thông tin sản phẩm, hàng hóa kinh doanh tại cửa hàng
-          </p>
-        </div>
-        <div className="flex flex-wrap justify-between space-x-4 gap-2 mr-4 items-center">
-          <div className="flex  space-x-4 mr-4 items-center">
-            <p className="text-left m-px mt-2 cursor-pointer hover:underline">
-              <IoCloudUploadOutline className="align-middle" /> Nhập file
-            </p>
-            <p className="text-left m-px mt-2 cursor-pointer  hover:underline">
-              <IoCloudDownloadOutline className="align-middle" /> Xuất file
-            </p>
-            <div className="w-px h-5 bg-gray-300 mt-2" />
-            <p className="text-left m-px mt-2 cursor-pointer  hover:underline">
-              <IoCloudDownloadOutline className="align-middle" /> Loại sản phẩm
-            </p>
-            <p className="text-left m-px mt-2 cursor-pointer  hover:underline">
-              <div className="relative inline-block">
-                <span className="absolute inset-0 bg-blue-600 opacity-0 hover:opacity-100 transition-opacity duration-300" />
-                <span className="relative z-10">
-                  {
-                    <DropdownListProduct
-                      label={
-                        <span className="flex items-center">
-                          Loại sản phẩm{" "}
-                          <FaCaretDown className="ml-1 text-slate-600" />
-                        </span>
-                      }
-                      items={[
-                        {
-                          label: "Tất cả",
-                          onClick: () => console.log("Tất cả"),
-                        },
-                        {
-                          label: "Điện thoại",
-                          onClick: () => console.log("Điện thoại"),
-                        },
-                        {
-                          label: "Máy tính",
-                          onClick: () => console.log("Máy tính"),
-                        },
-                      ]}
-                    />
-                  }
-                </span>
-              </div>
+          <div>
+            <p className="text-left m-px mt-2">
+              <h3 className="text-left m-px">Danh sách sản phẩm</h3>
+              Quản lý thông tin sản phẩm, hàng hóa kinh doanh tại cửa hàng
             </p>
           </div>
-          <div className="flex justify-end">
-            {/* Nút chính */}
-            <button className=" bg-blue-500 text-white rounded-l-sm hover:bg-blue-600 border border-blue-500">
-              <IoIosAdd className="align-middle w-7 h-7" />
-              Thêm sản phẩm
-            </button>
+          <div className="flex flex-wrap justify-between space-x-4 gap-2 mr-4 items-center">
+            <div className="flex  space-x-4 mr-4 items-center">
+              <p className="text-left m-px mt-2 cursor-pointer hover:underline">
+                <IoCloudUploadOutline className="align-middle" /> Nhập file
+              </p>
+              <p className="text-left m-px mt-2 cursor-pointer  hover:underline">
+                <IoCloudDownloadOutline className="align-middle" /> Xuất file
+              </p>
+              <div className="w-px h-5 bg-gray-300 mt-2" />
+              <p className="text-left m-px mt-2 cursor-pointer  hover:underline">
+                <IoCloudDownloadOutline className="align-middle" /> Loại sản
+                phẩm
+              </p>
+              <p className="text-left m-px mt-2 cursor-pointer  hover:underline">
+                <div className="relative inline-block">
+                  <span className="absolute inset-0 bg-blue-600 opacity-0 hover:opacity-100 transition-opacity duration-300" />
+                  <span className="relative z-10">
+                    {
+                      <DropdownListProduct
+                        label={
+                          <span className="flex items-center">
+                            Loại sản phẩm{" "}
+                            <FaCaretDown className="ml-1 text-slate-600" />
+                          </span>
+                        }
+                        items={[
+                          {
+                            label: "Tất cả",
+                            onClick: () => console.log("Tất cả"),
+                          },
+                          {
+                            label: "Điện thoại",
+                            onClick: () => console.log("Điện thoại"),
+                          },
+                          {
+                            label: "Máy tính",
+                            onClick: () => console.log("Máy tính"),
+                          },
+                        ]}
+                      />
+                    }
+                  </span>
+                </div>
+              </p>
+            </div>
+            <div className="flex justify-end">
+              {/* Nút chính */}
+              <button className=" bg-blue-500 text-white rounded-l-sm hover:bg-blue-600 border border-blue-500">
+                <IoIosAdd className="align-middle w-7 h-7" />
+                Thêm sản phẩm
+              </button>
 
-            {/* Nút mũi tên */}
-            <button className=" bg-blue-500 text-white rounded-r-sm  hover:bg-blue-600 border border-blue-500 border-l-0">
-              <DropdownListProduct
-                label=""
-                align="right"
-                items={[
-                  { label: "Tất cả", onClick: () => console.log("Tất cả") },
-                  {
-                    label: "Sản phẩm thường",
-                    onClick: () => <Link to="/products/add-product" />,
-                  },
-                  {
-                    label: "Thêm sản phẩm cho combo",
-                    onClick: () => <Link to="/products/add-combo" />,
-                  },
-                  {
-                    label: "Thêm sản phẩm có đơn vị quy đổi",
-                    onClick: () => <Link to="/products/add-conversion" />,
-                  },
-                  {
-                    label: "Thêm sản phẩm Serial/IMEI",
-                    onClick: () => <Link to="/products/add-serial" />,
-                  },
-                  {
-                    label: "Thêm sản phẩm quản lý theo lô - hạn sử dụng",
-                    onClick: () => <Link to="/products/add-batch" />,
-                  },
-                ]}
-              />
-            </button>
+              {/* Nút mũi tên */}
+              <button className=" bg-blue-500 text-white rounded-r-sm  hover:bg-blue-600 border border-blue-500 border-l-0">
+                <DropdownListProduct
+                  label=""
+                  align="right"
+                  items={[
+                    { label: "Tất cả", onClick: () => console.log("Tất cả") },
+                    {
+                      label: "Sản phẩm thường",
+                      onClick: () => <Link to="/products/add-product" />,
+                    },
+                    {
+                      label: "Thêm sản phẩm cho combo",
+                      onClick: () => <Link to="/products/add-combo" />,
+                    },
+                    {
+                      label: "Thêm sản phẩm có đơn vị quy đổi",
+                      onClick: () => <Link to="/products/add-conversion" />,
+                    },
+                    {
+                      label: "Thêm sản phẩm Serial/IMEI",
+                      onClick: () => <Link to="/products/add-serial" />,
+                    },
+                    {
+                      label: "Thêm sản phẩm quản lý theo lô - hạn sử dụng",
+                      onClick: () => <Link to="/products/add-batch" />,
+                    },
+                  ]}
+                />
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="bg-white marker:rounded-ms mt-4">
-        <ListProduct />
-        <ProductTable />
+        <div className="bg-white marker:rounded-ms mt-4">
+          <ListProduct />
+          <ProductTable />
+        </div>
       </div>
     </div>
   );
