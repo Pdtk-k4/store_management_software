0c5375b196fe0bf3a6e363218934583948421547
NguyenDuc1003 <ducmvb@gmail.com>
2025-10-03 09:12:56 +0700

    upload

--- frontend/src/pages/products/ProductDetails.tsx
+++ frontend/src/pages/products/ProductDetails.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FiChevronLeft } from "react-icons/fi";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import ProductInformation from "./components/ProductInformation";
+import ProductVariants from "./components/ProductVariants";
 const ProductDetails: React.FC = () => {
   return (
     <div>
